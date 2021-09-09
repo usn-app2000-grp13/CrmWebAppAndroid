875b09f4a4f3f95134f2c548b6cbb21010384096
Cato <otac@gmx.com>
2021-09-09 18:54:59 +0200

    test

---
+++ app/src/main/java/no/usn/gruppe4/crmwebappandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package no.usn.gruppe4.crmwebappandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
