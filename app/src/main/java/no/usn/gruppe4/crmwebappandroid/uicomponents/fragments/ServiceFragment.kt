8fb410beb7562e50ae10d9f862d58345dd6338b2
Trygve Johannessen <233512@usn.no>
2021-09-24 00:23:22 +0200

    servises oppdatering1

--- app/src/main/java/no/usn/gruppe4/crmwebappandroid/uicomponents/fragments/ServiceFragment.kt
+++ app/src/main/java/no/usn/gruppe4/crmwebappandroid/uicomponents/fragments/ServiceFragment.kt
@@ -5,17 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import no.usn.gruppe4.crmwebappandroid.R
-
+import no.usn.gruppe4.crmwebappandroid.databinding.FragmentLoginBinding
+import no.usn.gruppe4.crmwebappandroid.databinding.FragmentServiceBinding
 
 class ServiceFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_service, container, false)
+    lateinit var binding: FragmentServiceBinding
+
+    // 1) String tabell variabel
+    lateinit var tab: Array<String>
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentServiceBinding.inflate(inflater)
+
+        // 2) initierer string tabell
+        tab = resources.getStringArray(R.array.services)
+
+        // 3) arrayAdapter (Denne bruker vi til å koble tabellen til listviewet)
+        var arrayAdapter = activity?.let {
+            ArrayAdapter(
+                it,
+                android.R.layout.simple_list_item_1,
+                tab
+            )
+        }
+
+        // 4) kobbler arrayAdapteren til listviewet i xml filen
+        binding.listView.adapter = arrayAdapter
+
+        // 5) Lytter metode som registrerer klikk i listen
+        binding.listView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            test(tab[position])
+        }
+        return binding.root
+    }
+
+    private fun test(s: String) {
+        binding.textView.text = s
     }
 
 }
