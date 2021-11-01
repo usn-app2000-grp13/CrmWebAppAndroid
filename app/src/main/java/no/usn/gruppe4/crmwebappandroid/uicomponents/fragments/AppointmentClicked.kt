e886ff178a5c00dd7daeb9247c1a01d16f244b7b
Bjorn <b.keymis@gmail.com>
2021-11-01 18:59:53 +0100

    noe grafiske endringer

--- app/src/main/java/no/usn/gruppe4/crmwebappandroid/uicomponents/fragments/AppointmentClicked.kt
+++ app/src/main/java/no/usn/gruppe4/crmwebappandroid/uicomponents/fragments/AppointmentClicked.kt
@@ -1,8 +1,5 @@
 package no.usn.gruppe4.crmwebappandroid.uicomponents.fragments
 
-import android.app.DatePickerDialog
-import android.app.TimePickerDialog
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -10,24 +7,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import androidx.annotation.VisibleForTesting
-import androidx.core.view.isVisible
-import androidx.core.view.marginLeft
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
 import no.usn.gruppe4.crmwebappandroid.R
 import no.usn.gruppe4.crmwebappandroid.databinding.FragmentAppointmentClickedBinding
 import no.usn.gruppe4.crmwebappandroid.models.IdRequest
 import no.usn.gruppe4.crmwebappandroid.models.appointment.Appointment
-import no.usn.gruppe4.crmwebappandroid.models.appointment.Datasource
 import no.usn.gruppe4.crmwebappandroid.uicomponents.CalanderViewModel
-import org.w3c.dom.Text
 import java.text.SimpleDateFormat
-import java.util.*
-import kotlin.collections.ArrayList
-
 
 class AppointmentClicked : Fragment() {
 
@@ -84,24 +71,33 @@ class AppointmentClicked : Fragment() {
         binding.listService.removeAllViews()
         binding.ListEmployee.removeAllViews()
         for (i in appointment?.customers!!){
-            var customerElement = TextView(requireContext())
-            customerElement.text = "- ${i._customer?.firstname} ${i._customer?.lastname}"
-            customerElement.textSize = 15.0f
+            val customerElement = TextView(requireContext())
+            customerElement.text = "${i._customer?.firstname} ${i._customer?.lastname}"
+            customerElement.textSize = 18.0f
             customerElement.setPadding(30,10,10,10)
+            customerElement.setOnClickListener {
+                Log.i(TAG, "you clicked: ${i.toString()}")
+            }
             binding.listCustomer.addView(customerElement)
         }
         for (i in appointment?.employees!!){
-            var employeesElement = TextView(requireContext())
-            employeesElement.text = "- ${i._employee?.firstname} ${i._employee?.lastname}"
-            employeesElement.textSize = 15.0f
+            val employeesElement = TextView(requireContext())
+            employeesElement.text = "${i._employee?.firstname} ${i._employee?.lastname}"
+            employeesElement.textSize = 18.0f
             employeesElement.setPadding(30,10,10,10)
+            employeesElement.setOnClickListener {
+                Log.i(TAG, "you clicked: ${i.toString()}")
+            }
             binding.ListEmployee.addView(employeesElement)
         }
         for (i in appointment?.services!!){
-            var serviceElement = TextView(requireContext())
-            serviceElement.text = "- ${i._service?.name}"
-            serviceElement.textSize = 15.0f
+            val serviceElement = TextView(requireContext())
+            serviceElement.text = "${i._service?.name}"
+            serviceElement.textSize = 18.0f
             serviceElement.setPadding(30,10,10,10)
+            serviceElement.setOnClickListener {
+                Log.i(TAG, "you clicked: ${i.toString()}")
+            }
             binding.listService.addView(serviceElement)
         }
     }
