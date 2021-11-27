d877ce5d62f014fda282206574bb06ee2362f4e8
Bjorn <b.keymis@gmail.com>
2021-11-27 15:06:29 +0100

    added statistics

--- app/src/main/java/no/usn/gruppe4/crmwebappandroid/uicomponents/fragments/LandingFragment.kt
+++ app/src/main/java/no/usn/gruppe4/crmwebappandroid/uicomponents/fragments/LandingFragment.kt
@@ -2,6 +2,7 @@ package no.usn.gruppe4.crmwebappandroid.uicomponents.fragments
 
 import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -13,6 +14,11 @@ import android.widget.DatePicker
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
 import no.usn.gruppe4.crmwebappandroid.R
 import no.usn.gruppe4.crmwebappandroid.databinding.FragmentLandingBinding
 import no.usn.gruppe4.crmwebappandroid.models.appointment.Appointment
@@ -20,8 +26,12 @@ import no.usn.gruppe4.crmwebappandroid.models.customer.Customer
 import no.usn.gruppe4.crmwebappandroid.models.employee.Employee
 import no.usn.gruppe4.crmwebappandroid.models.login.SecSharePref
 import no.usn.gruppe4.crmwebappandroid.models.login.SharedPrefInterface
+import no.usn.gruppe4.crmwebappandroid.models.stats.employeePop
+import no.usn.gruppe4.crmwebappandroid.models.stats.servicePop
 import no.usn.gruppe4.crmwebappandroid.uicomponents.CalanderViewModel
 import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 
 class LandingFragment : Fragment(), DatePickerDialog.OnDateSetListener {
@@ -73,14 +83,32 @@ class LandingFragment : Fragment(), DatePickerDialog.OnDateSetListener {
             }
             nrApp = appointmentList.size
             Log.i("appointments in list", " $appointmentList")
-            binding.txtAppointmentToday.text = nrApp.toString()
-            binding.listAppointmentsToday.adapter = ArrayAdapter(requireContext(),android.R.layout.simple_spinner_dropdown_item, appointmentList)
             appointmentList.sortBy { it.timeindex }
         })
 
         viewModel.nrTodo.observe(viewLifecycleOwner, { todoCount ->
             nrTodo = todoCount
             binding.txtTodoCount.text = nrTodo.toString()
+        })
+
+        viewModel.statEmployee.observe(viewLifecycleOwner, {
+            val empPopData = mutableListOf<MyChartData>()
+            for (i in it){
+                if (i.employee.isNotEmpty()){
+                    empPopData.add(MyChartData(i.employee.toString(), i.count))
+                }
+            }
+            makePieChart(empPopData, "Employees", binding.piechart)
+        })
+
+        viewModel.statService.observe(viewLifecycleOwner, {
+            val serPopData = mutableListOf<MyChartData>()
+            for (i in it){
+                if (i.service.isNotEmpty()){
+                    serPopData.add(MyChartData(i.service.toString(), i.count))
+                }
+            }
+            makePieChart(serPopData, "Services", binding.piechartservice)
         })
 
 
@@ -104,6 +132,9 @@ class LandingFragment : Fragment(), DatePickerDialog.OnDateSetListener {
             findNavController().navigate(R.id.action_landingFragment_to_appointmentHistoryFragment)
         }
 
+
+
+
         // Inflate the layout for this fragment
         return binding.root
     }
@@ -120,6 +151,43 @@ class LandingFragment : Fragment(), DatePickerDialog.OnDateSetListener {
 
     fun goToSelectedDate(millis: Long){
         findNavController().navigate(R.id.action_landingFragment_to_calenderFragment)
+    }
+
+    data class MyChartData(
+        val label: String,
+        val amount: Int,
+    )
+
+    //Lager en pie chart ut av key value list
+    fun makePieChart(data: List<MyChartData>, title: String, pieChart: PieChart){
+        //chart data
+        val piechartListfin = ArrayList<Entry>()
+        val pieChartnames = ArrayList<String>()
+        val colorlist = ArrayList<Int>()
+        colorlist.add(Color.parseColor("#304567"))
+        colorlist.add(Color.parseColor("#309967"))
+        colorlist.add(Color.parseColor("#476567"))
+        colorlist.add(Color.parseColor("#890567"))
+        colorlist.add(Color.parseColor("#a35567"))
+        colorlist.add(Color.parseColor("#ff5f67"))
+        colorlist.add(Color.parseColor("#3ca567"))
+        var curindex = 0
+        for (i in data){
+            pieChartnames.add(i.label)
+            piechartListfin.add(Entry(i.amount.toFloat(), curindex))
+            curindex++
+        }
+        val pieDataSet = PieDataSet(piechartListfin, title)
+        pieDataSet.setColors(colorlist)
+        val pieData = PieData(pieChartnames, pieDataSet)
+        pieChart.data = pieData
+
+        pieChart.setDrawXValues(true)
+        pieChart.setDrawYValues(true)
+        pieChart.setValueTextColor(Color.BLUE)
+        pieChart.centerText = "Popularity"
+        pieChart.invalidate()
+        pieChart.animate()
     }
 
 
